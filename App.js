4c48045ce91630a0c3b52cf34a45e464a7ab31a3
Filip Parag <filiparag.com@gmail.com>
2018-09-08 22:39:25 +0200

    App

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
-import Home from './src/screens/Home'
-import News from './src/screens/News'
+import Home from './src/screens/Home';
+import ListWithImage from './src/screens/ListWithImage';
+import Article from './src/screens/Article';
+import Weather from './src/screens/Weather';
 
 export default class App extends React.Component {
   render() {
@@ -12,7 +14,9 @@ export default class App extends React.Component {
 const RootStack = createStackNavigator(
   {
     Home: Home,
-    News: News,
+    Weather: Weather,
+    ListWithImage: ListWithImage,
+    Article: Article,
   },
   {
     initialRouteName: 'Home',
